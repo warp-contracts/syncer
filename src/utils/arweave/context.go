ee9016e25bb4d0846046580a4be2aab5cb1e2bc2
Jan Olszak <jan@warp.cc>
2023-01-20 10:43:42 +0100

    Set atweave peer through the context

--- src/utils/arweave/context.go
+++ src/utils/arweave/context.go
@@ -5,4 +5,7 @@ type value string
 const (
 	// Setting this flag disables retrying request with peers
 	ContextDisablePeers = value("disablePeers")
+
+	// Forces the URL of the peer, otherwise the default one is picked
+	ContextForcePeer = value("forcePeer")
 )
