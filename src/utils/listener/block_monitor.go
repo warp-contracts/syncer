d7ad09b677c89e47218c05ff86499559e8d0e883
Jan Olszak <jan@warp.cc>
2023-03-06 10:52:22 +0100

    Release v0.1.47

--- src/utils/listener/block_monitor.go
+++ src/utils/listener/block_monitor.go
@@ -120,7 +120,7 @@ func (self *BlockMonitor) run() error {
 			}
 
 			if !block.IsValid() {
-				self.Log.WithField("height", height).Error("Block hash isn't valid")
+				self.Log.WithField("height", height).Error("Block hash isn't valid, retrying after sleep")
 				// self.Log.WithField("height", height).Error("Block hash isn't valid, blacklisting peer for ever and retrying")
 				self.monitor.Report.Errors.BlockValidationErrors.Inc()
 				time.Sleep(time.Second * 10)
