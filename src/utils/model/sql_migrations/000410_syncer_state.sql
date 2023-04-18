2f2ce701f034302e29d0c679729f916a21f6c14e
Jan Olszak <jan@warp.cc>
2023-04-18 10:45:35 +0200

    Fix block hash in migration

--- src/utils/model/sql_migrations/000410_syncer_state.sql
+++ src/utils/model/sql_migrations/000410_syncer_state.sql
@@ -12,5 +12,5 @@ CREATE TABLE IF NOT EXISTS sync_state
     CONSTRAINT check_block_height CHECK (finished_block_height > 422250)
 );
 
-INSERT INTO sync_state(name, finished_block_height, finished_block_hash) VALUES ('Interactions', 1161063, 'xPr-wXT3-0J6OQgnbCNgkid5H-rmaBwvZLtRhd3OAFo_stBTC-Af5kr3IVBnmxQb' );
-INSERT INTO sync_state(name, finished_block_height, finished_block_hash) VALUES ('Contracts', 1161063, 'xPr-wXT3-0J6OQgnbCNgkid5H-rmaBwvZLtRhd3OAFo_stBTC-Af5kr3IVBnmxQb' );
+INSERT INTO sync_state(name, finished_block_height, finished_block_hash) VALUES ('Interactions', 1161063, 'llxixWkllI9qzOVIjPhfGULFwtUmIkrCFusB3UIm8v_t1PKGlw87snWJ66lQdPcS' );
+INSERT INTO sync_state(name, finished_block_height, finished_block_hash) VALUES ('Contracts', 1161063, 'llxixWkllI9qzOVIjPhfGULFwtUmIkrCFusB3UIm8v_t1PKGlw87snWJ66lQdPcS' );
