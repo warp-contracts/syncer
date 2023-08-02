c271559c21280b495b773d845a8e5bae46717eaf
Jan Olszak <jan@warp.cc>
2023-08-02 18:09:21 +0200

    Fix validation of End and Start fields

--- src/gateway/request/get-interactions.go
+++ src/gateway/request/get-interactions.go
@@ -2,8 +2,8 @@ package request
 
 type GetInteractions struct {
 	SrcIds []string `json:"src_ids" binding:"min=0,max=100,dive,min=1,max=64"`
-	Start  uint     `json:"start"   binding:"gtfield=End"`
-	End    uint     `json:"end"     binding:"ltefield=Start"`
+	Start  uint     `json:"start"   binding:"ltefield=End"`
+	End    uint     `json:"end"     binding:"gtfield=Start"`
 	Limit  int      `json:"limit"   binding:"min=0,max=100000"`
 	Offset int      `json:"offset"  binding:"min=0,max=1000"`
 }
