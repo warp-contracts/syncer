ace81847d6d8c2828e4edc975a0524cfcd7bf502
Jan Olszak <jan@warp.cc>
2023-08-09 16:05:06 +0200

    Debug logs

--- src/gateway/server-interactions.go
+++ src/gateway/server-interactions.go
@@ -43,6 +43,7 @@ func (self *Server) onGetInteractions(db *gorm.DB) gin.HandlerFunc {
 			}
 
 			t := time.NewTimer(time.Duration(delta) * time.Millisecond)
+			LOG(c).WithField("delta", t).Debug("Waiting for the window to finish")
 			select {
 			case <-c.Done():
 				// Request is cancelled
@@ -51,6 +52,8 @@ func (self *Server) onGetInteractions(db *gorm.DB) gin.HandlerFunc {
 				// Window is finished, continue
 			}
 		}
+
+		LOG(c).WithField("start", in.Start).WithField("end", in.End).Debug("Get interactions")
 
 		var interactions []*model.Interaction
 
