0a9fba3e83ce00c5bc524bd9490194485f714639
Michał Szynwelski <szynek@warp.cc>
2023-11-06 10:36:13 +0100

    Restart method for watched task

--- src/utils/task/watchdog.go
+++ src/utils/task/watchdog.go
@@ -47,11 +47,16 @@ func (self *Watchdog) WithOnAfterStop(onAfterStop func()) *Watchdog {
 	return self
 }
 
-func (self *Watchdog) runPeriodic() (err error) {
+func (self *Watchdog) runPeriodic() error {
 	if self.isOK() {
-		return
+		return nil
 	}
 	self.Log.Warn("Watched task is not running, restarting")
+	return self.Restart()
+}
+
+func (self *Watchdog) Restart() (err error) {
+	self.Log.Warn("Restarting watched task, first stopping it")
 	self.watchedTask.StopWait()
 
 	self.Log.Warn("Watched task stopped, constructing again")
@@ -61,7 +66,7 @@ func (self *Watchdog) runPeriodic() (err error) {
 	err = self.watchedTask.Start()
 	if err != nil {
 		self.Log.WithError(err).Error("Failed to restart watched task")
-		panic(err)
+		return
 	}
 	self.Log.Warn("Watched task started")
 	return
